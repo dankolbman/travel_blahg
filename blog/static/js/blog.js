d2de22a4657ccc5f87f715e37bd07a9ded3064e5
Dan Kolbman <dankolbman@gmail.com>
2015-05-15 17:00:34 -0400

    Add styling

---
+++ blog/static/js/blog.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){   
+  $('.dropdown-button').dropdown({ hover: false });
+  $(".button-collapse").sideNav();
+  $('select').material_select();
+  $('.parallax').parallax();
+});
+
+
