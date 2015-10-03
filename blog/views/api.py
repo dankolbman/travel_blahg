53038e02213b1dbfc49c80aa98b6fd963037c731
dankolbman <dankolbman@gmail.com>
2015-10-03 01:27:00 -0400

    show more points on map

--- blog/views/api.py
+++ blog/views/api.py
@@ -13,10 +13,37 @@ api = Blueprint('api', __name__)
 def api_home():
   return 'hi'
 
+@api.route('/<string:username>/<string:api_key>/pings', methods=['GET', 'POST'])
+def pings(username, api_key):
+  data = request.get_json()
+  print(len(data['points']),'incoming points')
+  new = 0
+  dupes = 0
+  user = User.query.filter_by(username=username).first_or_404()
+  if user.api_key == api_key:
+    for point in data['points']:
+      ts = point['timestamp']/1000
+      ts = datetime.utcfromtimestamp(ts)
+      # Check if this point has already been reported
+      if not Ping.query.filter_by(author=user).filter_by(timestamp=ts).first():
+        ping = Ping(author=user, timestamp=ts,
+            accuracy=point['accuracy'],
+  	    loc='POINT({0} {1})'.format(point['longitude'],point['latitude']))
+        db.session.add(ping)
+        new += 1
+      else:
+        dupes += 1
+    db.session.commit()
+    print('New:',new,'Duplicate:',dupes)
+    return "Processed new points"
+  else:
+    print('Bad AUTH')
+    return "Bad AUTH"
+
 @api.route('/<string:username>/<string:api_key>/ping', methods=['GET', 'POST'])
 def ping(username, api_key):
   data = request.get_json()
-
+  print(data)
   user = User.query.filter_by(username=username).first_or_404()
   if user.api_key == api_key:
     ts = data['timestamp']/1000
@@ -27,12 +54,12 @@ def ping(username, api_key):
   	    loc='POINT({0} {1})'.format(data['longitude'],data['latitude']))
       db.session.add(ping)
       db.session.commit()
-      print("New ping")
-      return "Success"
+      return "New point"
     else:
       print("Duplicate ping")
       return "Duplicate point"
   else:
+    print('Bad AUTH')
     return "Bad AUTH"
 
 @api.route('/<string:username>/<string:api_key>', methods=['GET', 'POST'])
--- blog/views/user.py
+++ blog/views/user.py
@@ -6,7 +6,7 @@ from flask import Blueprint, render_template, flash, request, redirect, url_for,
 from flask.ext.login import login_required, current_user
 from werkzeug import secure_filename 
 
-from sqlalchemy import desc, func
+from sqlalchemy import desc, func, select
 from geoalchemy2 import Geometry
 
 from blog.extensions import cache
@@ -39,7 +39,7 @@ def prox(path):
 def ping_map():
   """ Show a map with users pings """
   import json
-  pings = Ping.query.filter_by(author_id=current_user.id).order_by(desc(Ping.timestamp)).limit(500).all()
+  pings = Ping.query.filter_by(author_id=current_user.id).order_by(desc(Ping.timestamp)).limit(10000).all()
 
   geo_json = []
   for ping in pings:
@@ -56,14 +56,16 @@ def ping_map_user(username):
   """ Show a map with users pings """
   import json
   user = User.query.filter_by(username=username).first_or_404();
-  pings = Ping.query.filter_by(author=user).order_by(desc(Ping.timestamp)).limit(500).all()
+  pings = Ping.query.filter_by(author=user).order_by(desc(Ping.timestamp))
+  pings = pings.filter(Ping.timestamp > '2015-06-03').limit(10000).all()
 
-  geo_json = []
+
+  geo_json = { "type": "LineString", "coordinates": [] }
   for ping in pings:
-    geom = json.loads(db.session.scalar(ping.loc.ST_AsGeoJSON()))
-    geo_json.append({"type":"Feature",'geometry':geom})
+    ping = json.loads(db.session.scalar(ping.loc.ST_AsGeoJSON()))
+    geo_json["coordinates"].append(ping["coordinates"])
 
-  last_up = pings[-1].timestamp
+  last_up = Ping.query.filter_by(author=user).order_by(desc(Ping.timestamp)).first().timestamp
   geo_json = json.dumps(geo_json)
 
   return render_template('user/ping_map.html', geo_json=geo_json,last=last_up,username=username)
